69df0e9c3a183fa9ea9f724daf40c4db70b854e1
SUNKI <daniel.sunki@gmail.com>
2026-03-07 00:15:23 +0900

    Implement selection sort algorithm in RISC-V assembly

--- ch4_p3.s
+++ ch4_p3.s
@@ -1,31 +1,110 @@
 .data
-arr: .word 1, 5, 3, 4, 10, 22, 2, 3, 44
+arr: .word 93,8,78,-6,51,49,3,2,128,0
 n:   .word 9
 
 .text
 .globl main
 main:
-    addi sp, sp, 10000
+  addi sp, sp, 10000
 
-    la a0, arr        # a0 = base address of arr
-    lw t1, n, t0      # t1 = length (9)
+  addi a0, x0, 0
+  
+  addi t0,x0,93
+  sw t0,0(a0) # take the value inside t0 and store it into the memory address 0(a0)
+  addi t0,x0,8
+  sw t0, 4(a0)
+  addi t0,x0,78
+  sw t0, 8(a0)
+  addi t0,x0-6,
+  sw t0, 12(a0)
+  addi t0,x0,51
+  sw t0, 16(a0)
+  addi t0,x0,49
+  sw t0, 20(a0)
+  addi t0,x0,3
+  sw t0, 24(a0)
+  addi t0,x0,2
+  sw t0, 28(a0)
+  addi t0,x0,128
+  sw t0, 32(a0)
+  addi t0,x0,0
+  sw t0, 36(a0)
 
-    # Access arr[i] like Python::
-    
-    # arr[0] → lw t0, 0(a0)
-    # arr[1] → lw t0, 4(a0)
-    # arr[i] → multiply i by 4, then lw t0, 0(a0) after add
+  addi a1, x0, 10
 
-    # Example: loop through arr[i]
-    addi t2, x0, 0        # i = 0
-loop:
-    bge t2, t1, done      # if i >= n, exit
+  jal ra, SEL_SORT
+  jal ra, FIN 
+SEL_SORT:
+  addi sp,sp, -8
+  sw ra, 4(sp)
+  sw s0, 0(sp)
 
-    slli t3, t2, 2        # t3 = i * 4  (byte offset)
-    add  t4, a0, t3       # t4 = &arr[i]
-    lw   t5, 0(t4)        # t5 = arr[i]  ← this is your "arr[i]"
+  addi t0, x0, 0 # i 
+  addi t1, x0, 0 # j
+  addi t2, x0, 0 # min_index
 
-    addi t2, t2, 1        # i++
-    j loop
+  addi s0, a1, 0 # store n( = 10)
+  addi a1, a1, -1 # n-1( = 9)
 
-done:
+UNSORTED_ARRAY_BOUNDARY_LOOP:
+  beq t0, a1, END_UNSORTED_ARRAY_BOUNDARY_LOOP # while i < (n-1)
+
+  addi t2, t0, 0 # min_index = i 
+  addi t1, t0, 1 #j = i+1
+
+SUBARRAY_LOOP:
+  beq t1, s0, END_SUBARRAY_LOOP # while j < n 
+
+  slli t5, t1, 2 # j*sizeof(int)
+  add t6, a0, t5 
+  lw t4, 0(t6) # load arr[j]
+
+  slli t5, t2, 2 # min_index*sizeof(int)
+  add t6, a0, t5
+  lw t3, 0(t6)
+
+  blt t3, t4, MIN_REMAINS_SAME # if arr[min_index] < arr[j], min_index remains the same    
+  addi t2, t1, 0 # min_index = j
+
+MIN_REMAINS_SAME:
+  addi t1, t1, 1 # j = j+1
+  beq x0, x0,SUBARRAY_LOOP
+
+END_SUBARRAY_LOOP:
+  slli t5, t0,2 # min_index*sizeof(int)
+  add t6, a0, t5 # arr[min_index]
+  lw t3, 0(t6)
+
+  slli t1, t0, 2 # i*sizeof(int)
+  add t1, t1, a0 # arr[i]
+  lw t4, 0(t1)
+
+  sw t3, 0(t1)
+  sw t4, 0(t6)
+
+  addi t0, t0, 1 # i = i+1
+  beq x0, x0, UNSORTED_ARRAY_BOUNDARY_LOOP
+
+
+END_UNSORTED_ARRAY_BOUNDARY_LOOP:
+  lw s0, 0(sp)
+  lw ra, 4(sp)
+  addi sp,sp, 8
+  jalr x0,ra,0
+
+
+FIN:
+  li a7, 4
+  la a0, ans
+  ecall
+
+  li a7, 1
+  mv a0, t1 # char to print
+  ecall
+
+  li a7,4 # print "\n"
+  la a0, newline
+  ecall
+  
+  li a7, 10 # exit
+  ecall
